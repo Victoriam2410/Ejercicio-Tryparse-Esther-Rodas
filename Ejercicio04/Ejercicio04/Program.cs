3cc6e952c39f7c921b341dab108eb6a0b9376a88
Victoriam2410 <estherrodas61@gmail.com>
2026-03-21 23:12:59 -0600

    Add files via upload

---
+++ Ejercicio04/Ejercicio04/Program.cs
@@ -0,0 +1,10 @@
+﻿int anioActual = 2026;
+int anioNacimiento;
+
+Console.Write("Ingrese su año de nacimiento: ");
+while (!int.TryParse(Console.ReadLine(), out anioNacimiento) || anioNacimiento < 1900 || anioNacimiento > anioActual)
+{
+    Console.WriteLine($" Ingrese un año válido entre 1900 y {anioActual}.");
+}
+
+Console.WriteLine($"Año de nacimiento ingresado: {anioNacimiento}");

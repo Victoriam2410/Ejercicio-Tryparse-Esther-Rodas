2858da906d10d3b0791d51b50d3dc5123e78ba44
Victoriam2410 <estherrodas61@gmail.com>
2026-03-21 23:11:23 -0600

    Add files via upload

---
+++ Ejercicio02/Ejercicio02/Program.cs
@@ -0,0 +1,8 @@
+﻿decimal precio;
+Console.Write("Ingrese el precio del producto: ");
+while (!decimal.TryParse(Console.ReadLine(), out precio) || precio <= 0)
+{
+    Console.WriteLine("Error: Ingrese un precio decimal válido mayor que 0.");
+    Console.Write("Ingrese el precio del producto: ");
+}
+Console.WriteLine($"Precio ingresado: {precio}");

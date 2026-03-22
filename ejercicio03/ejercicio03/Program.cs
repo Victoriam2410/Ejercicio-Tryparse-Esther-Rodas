448d96505a63fe0a92fd1244c2df76d89d7ceedc
Victoriam2410 <estherrodas61@gmail.com>
2026-03-21 23:12:30 -0600

    Add files via upload

---
+++ ejercicio03/ejercicio03/Program.cs
@@ -0,0 +1,7 @@
+﻿int cantidad;
+Console.Write("Ingrese la cantidad de estudiantes: ");
+while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
+{
+    Console.WriteLine("Ingrese un número entero positivo.");
+}
+Console.WriteLine($"Cantidad de estudiantes ingresada: {cantidad}");

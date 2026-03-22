998da2282187dbdfda4ec05ddbe5fe1727c48b5e
Victoriam2410 <estherrodas61@gmail.com>
2026-03-21 23:14:28 -0600

    Add files via upload

---
+++ Ejercicio05/Ejercicio05/Program.cs
@@ -0,0 +1,15 @@
+﻿int numero;
+Console.Write("Ingrese un número entero: ");
+while (!int.TryParse(Console.ReadLine(), out numero))
+{
+    Console.WriteLine("Ingrese un número entero válido.");
+}
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine($"El número {numero} es par.");
+}
+else
+{
+    Console.WriteLine($"El número {numero} es impar.");
+}
---
+++ Ejercicio06/Ejercicio06/Program.cs
@@ -0,0 +1,8 @@
+﻿DateTime fecha;
+Console.Write("Ingrese una fecha (dd/mm/aaaa): ");
+while (!DateTime.TryParse(Console.ReadLine(), out fecha))
+{
+    Console.WriteLine("Ingrese una fecha válida.");
+}
+
+Console.WriteLine($"Fecha ingresada: {fecha}");

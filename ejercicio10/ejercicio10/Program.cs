140e1769f5922f520cb4e13b929b38fb4e21f891
Victoriam2410 <estherrodas61@gmail.com>
2026-03-21 23:18:46 -0600

    Add files via upload

---
+++ ejercicio10/ejercicio10/Program.cs
@@ -0,0 +1,13 @@
+﻿int cantidad;
+Console.Write("Ingrese la cantidad de productos a comprar: ");
+while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
+{
+    Console.WriteLine("Ingrese un número entero positivo.");
+}
+
+for (int i = 1; i <= cantidad; i++)
+{
+    Console.WriteLine($"Producto {i} ingresado correctamente.");
+}
+
+Console.WriteLine($"\nSe han procesado {cantidad} productos.");

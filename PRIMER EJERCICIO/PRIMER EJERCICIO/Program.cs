a3e9cf77dfb375ebcf253527943f149fcb0e7bdf
Victoriam2410 <estherrodas61@gmail.com>
2026-03-21 23:10:36 -0600

    Add files via upload

---
+++ PRIMER EJERCICIO/PRIMER EJERCICIO/Program.cs
@@ -0,0 +1,7 @@
+﻿int edad;
+Console.Write("Ingrese la edad: ");
+while (!int.TryParse(Console.ReadLine(), out edad) || edad <0)
+{
+    Console.WriteLine("ingrese una edad valida.");
+}
+Console.WriteLine($"Edad ingresada: {edad}");

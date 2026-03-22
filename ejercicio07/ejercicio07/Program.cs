1e3a5bd39a4d5bc7373b7f915fc8c14f9107b67d
Victoriam2410 <estherrodas61@gmail.com>
2026-03-21 23:16:26 -0600

    Add files via upload

---
+++ ejercicio07/ejercicio07/Program.cs
@@ -0,0 +1,8 @@
+﻿decimal salario;
+Console.Write("Ingrese el salario del empleado: ");
+while (!decimal.TryParse(Console.ReadLine(), out salario) || salario < 0)
+{
+    Console.WriteLine("Ingrese un salario decimal válido mayor o igual a 0.");
+}
+
+Console.WriteLine($"Salario ingresado: {salario}");

80370d9af21c9fa832ae6ce9283386f326bee135
Victoriam2410 <estherrodas61@gmail.com>
2026-03-21 23:17:39 -0600

    Add files via upload

---
+++ ejercicio09/ejercicio09/Program.cs
@@ -0,0 +1,15 @@
+﻿double nota;
+Console.Write("Ingrese la nota del estudiante (0-100): ");
+while (!double.TryParse(Console.ReadLine(), out nota) || nota < 0 || nota > 100)
+{
+    Console.WriteLine("Ingrese una nota válida entre 0 y 100.");
+}
+
+if (nota >= 60)
+{
+    Console.WriteLine($"Nota ingresada: {nota} Aprobado.");
+}
+else
+{
+    Console.WriteLine($"Nota ingresada: {nota} Reprobado.");
+}

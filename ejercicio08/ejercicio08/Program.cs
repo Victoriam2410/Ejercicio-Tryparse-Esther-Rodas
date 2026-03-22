8f4b7511c5a54c9d25059c11e975db164715481a
Victoriam2410 <estherrodas61@gmail.com>
2026-03-21 23:17:15 -0600

    Add files via upload

---
+++ ejercicio08/ejercicio08/Program.cs
@@ -0,0 +1,9 @@
+﻿double numero;
+Console.Write("Ingrese un número para calcular su raíz cuadrada: ");
+while (!double.TryParse(Console.ReadLine(), out numero) || numero < 0)
+{
+    Console.WriteLine("Ingrese un número válido mayor o igual a 0.");
+}
+
+double raiz = Math.Pow(numero, 0.5);
+Console.WriteLine($"La raíz cuadrada es: {raiz}");
